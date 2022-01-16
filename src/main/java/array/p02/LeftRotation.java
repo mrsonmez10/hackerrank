24095f79e8695aec6d62965b06cf61c3cb689d78
frknsnmz <furkan.sonmez@bilgiedu.net>
2022-01-16 23:10:43 +0300

    Left rotation example solved.

--- src/main/java/array/p02/LeftRotation.java
+++ src/main/java/array/p02/LeftRotation.java
@@ -1,8 +1,36 @@
 package array.p02;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LeftRotation{
 
+    public static List<Integer> rotLeft(List<Integer> a, int d){
+        int sizeOfa = a.size();
+        List<Integer> nTimesShiftedList = new ArrayList<>(); // giving size is not must for arraylists
+        for (int i = 0; i < a.size(); i++) {
+            nTimesShiftedList.add(0);
+        }
+        if(isShiftable(a)){
+            for(int i = 0; i < sizeOfa; i++){
+                int diff = i - d;
+                if(diff >= 0){
+                    nTimesShiftedList.set(diff, a.get(i));
+                } else{
+                    int index = diff + sizeOfa;
+                    nTimesShiftedList.set(index, a.get(i));
+                }
+            }
+        }
+        return nTimesShiftedList;
+    }
 
-
+    public static boolean isShiftable(List<Integer> a){
+        if(a.size() == 1 || a.isEmpty()){
+            return false;
+        }
+        return true;
+    }
 
 }
--- src/main/java/array/p02/TestLeftRotation.java
+++ src/main/java/array/p02/TestLeftRotation.java
@@ -1,8 +1,14 @@
 package array.p02;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class TestLeftRotation{
 
     public static void main(String[] args){
+
+        List<Integer> rotList = Arrays.asList(33 ,47 ,70 ,37 ,8 ,53 ,13 ,93 ,71 ,72 ,51 ,100 ,60 ,87 ,97);
+        System.out.println("Left rotation : new array after rotation : " + LeftRotation.rotLeft(rotList, 13));
 
     }
 
